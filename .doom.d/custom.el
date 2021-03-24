21c9d51b423d055e1a9ca2c6e5bc3cfd3080df8a
offendo <nilaypatel2@gmail.com>
2021-03-23 21:18:19 -0400

    added my old doom config

---
+++ .doom.d/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("28c56a3c5e503137ddbd75c09550955bf9138839289c5daa535a5adba3b068dd" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
